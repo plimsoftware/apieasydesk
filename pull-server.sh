f130f67614d026baab30451457f6bd0fa1c905aa
Luís Fernandes <64134739+plimsoftware@users.noreply.github.com>
2020-08-14 12:30:07 +0100

    Delete pull-server.sh

--- pull-server.sh
+++
@@ -1,9 +0,0 @@
-npm run build
-git add .
-
-git commit -am "test two git"
-git push apieasydesk master
-ssh agenda.plimsoftware.pt \
-  'git -C /home/miguel/apieasydesk/api/ ' \
-  'pull apieasydesk master && ' \
-  'pm2 restart apieasydesk && sudo systemctl restart nginx'
